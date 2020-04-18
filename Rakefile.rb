b6fd3503ea7cb3f71a5a6821dc52b638be4ac02d
Tim "S.D.Eagle" Zeitz <dev.tim.zeitz@gmail.com>
2020-04-18 14:12:11 +0200

    script experiments

---
+++ Rakefile.rb
@@ -0,0 +1,162 @@
+raise "No or wrong experiments output dir configured - please set CHPOT_EXP_OUTPUT_DIR env var" unless Dir.exists? exp_dir || ""
+exp_dir = exp_dir
+
+file "paper/ch_potentials.pdf" => [
+  "paper/ch_potentials.tex"] do
+
+  Dir.chdir "paper" do
+    sh "latexmk -pdf ch_potentials.tex"
+  end
+end
+
+task default: "paper/ch_potentials.pdf"
+
+namespace "fig" do
+  # file "paper/fig/ranks.pdf" => FileList["#{exp_dir}/rank_queries_all_graphs/*.json"] + FileList["#{exp_dir}/rank_query_stats_no_astar_all_graphs/*.json"] + ['eval/rank_plot.py'] do
+  #   sh "eval/rank_plot.py"
+  # end
+end
+
+namespace "table" do
+  # file "paper/table/related_work.tex" => FileList[
+  #   "#{exp_dir}/katch/contraction/*.out",
+  #   "#{exp_dir}/katch/uniform_queries/*.out",
+  #   "#{exp_dir}/tds/contraction/*.out",
+  #   "#{exp_dir}/tds/uniform_queries/*.out",
+  #   "#{exp_dir}/queries_all_graphs/*.json",
+  #   "#{exp_dir}/customization_all_graphs/*.json",
+  #   "#{exp_dir}/partition/*.out",
+  #   "#{exp_dir}/contraction/*.json",
+  #   ] + ["eval/related_work_cmp.py"] do
+
+  #   sh "eval/related_work_cmp.py"
+  # end
+end
+
+osm_graph = "/algoDaten/zeitz/roadgraphs/osm_ger_rel/"
+dimacs_graph = "/algoDaten/zeitz/roadgraphs/europe/"
+
+static_graphs = [osm_graph, dimacs_graph]
+td_graphs = [
+  "/algoDaten/graphs/cleaned_td_road_data/ptv17-eur-car/day/di/",
+  "/algoDaten/graphs/cleaned_td_road_data/de/day/dido/",
+]
+graphs = static_graphs + td_graphs
+
+live_dir = '/algoDaten/mapbox/live-speeds/2019-08-02-15:41/*'
+
+namespace "prep" do
+  directory osm_graph
+  file osm_graph => "code/osm_import/build/import_osm" do
+    wd = Dir.pwd
+    Dir.chdir osm_graph do
+      sh "#{wd}/code/osm_import/build/import_osm /algoDaten/zeitz/roadgraphs/germany-latest.osm.pbf #{Dir[live_dir + '*'].join(' ')}"
+    end
+  end
+
+  graphs.each do |graph|
+    directory graph + "lower_bound_ch"
+    file graph + "lower_bound_ch" => ["code/compute_ch/build/compute_ch", graph + 'lower_bound'] do
+      sh "code/compute_ch/build/compute_ch #{graph}/first_out #{graph}/head #{graph}/lower_bound "
+          + "#{graph}/lower_bound_ch/order "
+          + "#{graph}/lower_bound_ch/forward_first_out #{graph}/lower_bound_ch/forward_head #{graph}/lower_bound_ch/forward_weight "
+          + "#{graph}/lower_bound_ch/backward_first_out #{graph}/lower_bound_ch/backward_head #{graph}/lower_bound_ch/backward_weight"
+    end
+  end
+
+  static_graphs.each do |graph|
+    file graph + "lower_bound" do
+      sh "ln -s #{graph}/travel_time #{graph}/lower_bound"
+    end
+  end
+
+  td_graphs.each do |graph|
+    file graph + "lower_bound" do
+      Dir.chdir "code/bmw_routing_engine/engine" do
+        sh "cargo run --release --bin td_lower_bound -- #{graph}"
+      end
+    end
+  end
+end
+
+namespace "exp" do
+  desc "Run all experiments"
+  task all: [:preprocessing, :scaled_weights, :building_blocks, :applications, :baselines]
+
+  directory "#{exp_dir}/preprocessing"
+  directory "#{exp_dir}/scaled_weights"
+  directory "#{exp_dir}/building_blocks"
+  directory "#{exp_dir}/applications"
+
+  task preprocessing: ["code/compute_ch/build/compute_ch", "#{exp_dir}/preprocessing"] + graphs.map { |g| g + 'lower_bound' } do
+    graph.each do |graph|
+      5.times do
+        filename = "#{exp_dir}/preprocessing/" + `date --iso-8601=seconds`.strip + '.out'
+        sh "echo '#{graph}' >> #{filename}"
+        sh "code/compute_ch/build/compute_ch #{graph}/first_out #{graph}/head #{graph}/lower_bound "
+          + "#{graph}/lower_bound_ch/order "
+          + "#{graph}/lower_bound_ch/forward_first_out #{graph}/lower_bound_ch/forward_head #{graph}/lower_bound_ch/forward_weight "
+          + "#{graph}/lower_bound_ch/backward_first_out #{graph}/lower_bound_ch/backward_head #{graph}/lower_bound_ch/backward_weight "
+          + ">> #{filename}"
+      end
+    end
+  end
+
+  task scaled_weights: [dimacs_graph + 'lower_bound_ch'] + ["#{exp_dir}/scaled_weights"] do
+    Dir.chdir "code/bmw_routing_engine/engine" do
+      sh "cargo run --release --bin chpot_weight_scaling -- #{graph} > #{exp_dir}/scaled_weights/$(date --iso-8601=seconds).json"
+    end
+  end
+
+  task building_blocks: [dimacs_graph + 'lower_bound_ch'] + ["#{exp_dir}/building_blocks"] do
+    Dir.chdir "code/bmw_routing_engine/engine" do
+      sh "cargo run --release --bin dijkstra -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-only-topo chpot-no-scc chpot-no-deg2 chpot-no-deg3' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-only-topo chpot-no-deg2 chpot-no-deg3' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-only-topo chpot-no-deg3' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-only-topo' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-no-scc chpot-no-deg2 chpot-no-deg3' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-no-deg2 chpot-no-deg' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale --features 'chpot-no-deg3' -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simple_scale -- #{graph} > #{exp_dir}/building_blocks/$(date --iso-8601=seconds).json"
+    end
+  end
+
+  task applications: graphs.map { |g| g + 'lower_bound_ch' } + ["#{exp_dir}/applications"] do
+    Dir.chdir "code/bmw_routing_engine/engine" do
+      td_graphs.each do |graph|
+        sh "cargo run --release --bin td_dijkstra -- #{graph} > #{exp_dir}/applications/$(date --iso-8601=seconds).json"
+        sh "cargo run --release --bin chpot_td -- #{graph} > #{exp_dir}/applications/$(date --iso-8601=seconds).json"
+      end
+      static_graphs.each do |graph|
+        sh "cargo run --release --bin dijkstra -- #{graph} > #{exp_dir}/applications/$(date --iso-8601=seconds).json"
+      end
+      sh "cargo run --release --bin chpot_live -- #{osm_graph} #{live_dir} > #{exp_dir}/applications/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_blocked -- #{osm_graph} > #{exp_dir}/applications/$(date --iso-8601=seconds).json"
+      sh "cargo run --release --bin chpot_simulated_live -- #{dimacs_graph} > #{exp_dir}/applications/$(date --iso-8601=seconds).json"
+    end
+  end
+end
+
+namespace 'build' do
+  task :osm_import => "code/osm_import/build/import_osm"
+
+  directory "code/osm_import/build"
+
+  file "code/osm_import/build/import_osm" => "code/osm_import/build" do
+    Dir.chdir "code/osm_import/build/" do
+      sh "cmake -DCMAKE_BUILD_TYPE=Release .. && make"
+    end
+  end
+
+  task :compute_ch => "code/compute_ch/build/compute_ch"
+
+  directory "code/compute_ch/build"
+
+  file "code/compute_ch/build/compute_ch" => "code/compute_ch/build" do
+    Dir.chdir "code/compute_ch/build/" do
+      sh "cmake -DCMAKE_BUILD_TYPE=Release .. && make"
+    end
+  end
+end
+
